a983720b47fb54abea9637ce638b9851ad84d9e1
Akhil Vinayak <akhilvinayak@Akhils-MacBook-Air.local>
2023-03-10 17:57:52 +0530

    fix: added redux boilerplate

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -27,7 +27,7 @@ const bundle = async (rawCode: string) => {
       code: result.outputFiles[0].text,
       err: '',
     };
-  } catch (err) {
+  } catch (err:any) {
     return {
       code: '',
       err: err.message,
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
+
 
 
 const App = () => {
   return (
+    <Provider store={store}>
     <div>
       <TextEditor />
     </div>
+    </Provider>
   );
 };
 
